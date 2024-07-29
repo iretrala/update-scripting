8e6eb54884d82360d63f2dfaecb253ed0ec5916b
Andrew Larter <dru.larter@me.com>
2024-07-29 09:45:28 -0300

    first commit

---
+++ whatsnew-rhel.sh
@@ -0,0 +1,55 @@
+#!/bin/zsh
+
+# Function to update and upgrade using dnf for Fedora/CentOS/RHEL
+update_rhel_fedora() {
+    echo "Updating and upgrading RHEL/Fedora system using dnf..."
+    sudo dnf update -y
+    sudo dnf upgrade -y
+}
+
+# Function to update and upgrade using apt for Ubuntu/Kali/Debian
+update_ubuntu() {
+    echo "Updating and upgrading Ubuntu system using apt..."
+    sudo apt update -y
+    sudo apt upgrade -y
+}
+
+# Function to update and upgrade using Homebrew for macOS (or Linux if installed)
+update_homebrew() {
+    echo "Updating and upgrading using Homebrew..."
+
+    # Check if Homebrew is installed
+    if command -v brew &> /dev/null; then
+        echo "Homebrew is installed."
+        echo ""
+        echo "Updating Homebrew..."
+        brew update
+        echo ""
+        echo "Upgrading packages managed by Homebrew"
+        brew upgrade
+        echo ""
+        echo "Cleaning up packages managed by Homebrew"
+        brew cleanup
+    else
+        echo "Homebrew is not installed. Skipping Homebrew updates."
+    fi
+}
+
+# Determining the OS
+if [ "$(uname)" = "Darwin" ]; then
+    # macOS
+    update_homebrew
+elif [ -f /etc/redhat-release ] || [ -f /etc/fedora-release ]; then
+    # RHEL or Fedora
+    update_rhel_fedora
+    update_homebrew
+elif [ -f /etc/lsb-release ]; then
+    # Ubuntu
+    update_ubuntu
+    update_homebrew
+else
+    echo "This OS is not supported using this script."
+    exit 1
+fi
+
+echo "System updates and upgrades completed."
